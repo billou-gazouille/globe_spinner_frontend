346571cc75c5a79751c735f15cfbf690a34f3e56
William Joannes <william.joannes@outlook.com>
2023-12-16 17:31:17 +0100

    commencé a styliser le SelectedSuggestionScreen

--- screens/SelectedSuggestionsScreen.js
+++ screens/SelectedSuggestionsScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { View, StyleSheet, Text, StatusBar } from "react-native";
+import { View, StyleSheet, Text, StatusBar, Image, ScrollView } from "react-native";
 import BackButton from "../components/BackButton";
+import { CustomText } from "../components/CustomText";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
 
 export default function SelectedSuggestionsScreen({ navigation, route }) {
   
@@ -9,22 +12,88 @@ export default function SelectedSuggestionsScreen({ navigation, route }) {
   const {trip} = route.params;
   
   return (
-    <View style={styles.container}>
+    
+    <ScrollView contentContainerStyle={styles.container}>
       <StatusBar style="auto" />
-      <Text style={styles.text}>Selected Suggestions Screen</Text>
-      <Text style={styles.text}>{trip}</Text>
+      <Image source={require('../assets/noImage.jpg')} style={styles.cityImg}></Image>
+      
+      <View style={styles.headingAndSectionPair}>
+        <CustomText style={styles.text}>Accommodation</CustomText>
+        <View style={styles.accommodationDetailsContainer}>
+          <CustomText style={styles.accommodationName}>Hostel Espresso City Center</CustomText>
+          <View style={{flex: 1, flexDirection: 'row', width: '40%'}}>
+            <View style={{flex: 1}}>
+              <View style={{marginLeft: 40, marginTop: 15, flexDirection: 'row'}}>
+                <FontAwesome name='users' size={25} color='black'/>
+                <CustomText style={{marginLeft: 10, fontSize: 18}}>3</CustomText>
+              </View>
+              <View style={{marginLeft: 40, marginTop: 20, flexDirection: 'row'}}>
+                <FontAwesome name='timer' size={25} color='black'/>
+                <CustomText style={{marginLeft: 10, fontSize: 18}}>4 nights</CustomText>
+              </View>
+            </View>
+            {/* <View style={{marginLeft: 40, marginTop: 20}}>
+              <FontAwesome name='map-marker' size={25} color='black'/>
+              <CustomText style={{marginLeft: 10, fontSize: 16}}>Overtoom 57, 1054 HC</CustomText>
+            </View> */}
+            <FontAwesome name='map-marker' size={25} color='black'/>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.headingAndSectionPair}>
+        <CustomText style={styles.text}>Activities</CustomText>
+        <View style={styles.accommodationDetailsContainer}>
+        </View>
+      </View>
+
+      <View style={styles.headingAndSectionPair}>
+        <CustomText style={styles.text}>Transports</CustomText>
+        <View style={styles.accommodationDetailsContainer}>
+        </View>
+      </View>
+
+      {/* <Text style={styles.text}>Selected Suggestions Screen</Text> */}
+      <CustomText>Sisi is a cutie</CustomText>
+      <CustomText style={styles.text}>{trip}</CustomText>
       <BackButton navigation={navigation} />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    // flex: 1,
+    justifyContent: "space-between",
     alignItems: "center",
   },
   text: {
     fontSize: 28,
+  },
+  cityImg: {
+    width: '100%',
+    height: 150,
+  },
+  accommodationDetailsContainer: {
+    width: '95%',
+    height: 150,
+    // color: '#ECECEC',
+    shadowColor: 'black',
+    elevation: 10,
+    backgroundColor: '#DCDCDC',
+    borderRadius: 25,
+  },
+  headingAndSectionPair: {
+    width: '100%',
+    height: 200,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  accommodationName: {
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 10,
   },
 });
